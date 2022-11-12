0efe88f6a4c553ab94caa9ef18fe9a858f1d7ec9
heartgg <a.szumski98@gmail.com>
2022-11-12 17:02:13 -0600

    get prof cloud

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import functions_framework
 
 @functions_framework.http
-def hello_http(request):
+def get_professor(request):
    """HTTP Cloud Function.
    Args:
        request (flask.Request): The request object.
