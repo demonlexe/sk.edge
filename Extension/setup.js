fb9131842fdf63c11c01acf2587c083113acbe35
Alexis Kaufman <alexiskaufman@gmail.com>
2022-11-12 15:10:18 -0600

    add setup-complete logic, for when user has completed setup, so they don't have to re-do it

--- Extension/setup.js
+++ Extension/setup.js
@@ -3,10 +3,17 @@ function setupClicked() {
     let gpa = $("#input-gpa").val();
     let school = $("#input-school").val();
     if (gpa.length >= 1 && school.length >= 3) {
-        console.log(gpa, school);
         let setupPg = $("#setup-page").detach();
-    $("#dashboard-page").removeClass("hiddenElement");
-        //FIXME; store this somewhere
+         $("#dashboard-page").removeClass("hiddenElement");
+        chrome.storage.sync.set({student_gpa: gpa}, function() {
+            console.log('student_gpa is set to ' + gpa);
+        });
+        chrome.storage.sync.set({student_school: school}, function() {
+            console.log('student_school is set to ' + school);
+        });
+        chrome.storage.sync.set({user_setup_complete: true}, function() {
+            console.log('user_setup_complete is set to true');
+        });
     } else {
         if (gpa.length < 1) {
             $("#input-gpa").addClass("highlightedElement");
@@ -22,6 +29,14 @@ function checkLength(elem, num) {
         elem.removeClass("highlightedElement");
     }
 }
+
+chrome.storage.sync.get('user_setup_complete', function(result) {
+    if (result.user_setup_complete) {
+        // Then user has already completed setup and we have their data.
+        let setupPg = $("#setup-page").detach();
+        $("#dashboard-page").removeClass("hiddenElement");
+    }
+});
 
 $("#send-button").on("click", setupClicked);
 let gpaInput = $("#input-gpa");
