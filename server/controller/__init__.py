f1b90d315258a91b26f5b903058ecc907d81d5d8
heartgg <a.szumski98@gmail.com>
2022-11-12 17:45:12 -0600

    change things

--- server/controller/__init__.py
+++ server/controller/__init__.py
@@ -2,11 +2,13 @@ from fix_ratemyprofessor import get_professor_by_school_and_name, get_school_by_
 
 def get_professor_ctrl(school_name: str, prof_name: str):
     school = get_school_by_name(school_name)
+    if school_name is None or prof_name is None:
+        return None, 404
     if school is None:
         print("School", school_name, "not found!")
-        return None
+        return None, 404
     prof: Professor = get_professor_by_school_and_name(school, prof_name)
     if prof is None:
         print("Prof", prof_name, "not found!")
-        return None
+        return None, 404
     return prof.__serialize__() 
