1fe6b57287ac8a85651bad3865f812e3a306566a
heartgg <a.szumski98@gmail.com>
2022-11-12 17:08:02 -0600

    add get prof function

--- server/main.py
+++ server/main.py
@@ -1,23 +1,10 @@
 import functions_framework
 
+from server.controller import get_professor_ctrl
+
 @functions_framework.http
 def get_professor(request):
-   """HTTP Cloud Function.
-   Args:
-       request (flask.Request): The request object.
-       <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-   Returns:
-       The response text, or any set of values that can be turned into a
-       Response object using `make_response`
-       <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-   """
-   request_json = request.get_json(silent=True)
-   request_args = request.args
+   # request_json = request.get_json(silent=True)
+   response = get_professor_ctrl(request.args.get("school"), request.args.get("name"))
 
-   if request_json and 'name' in request_json:
-       name = request_json['name']
-   elif request_args and 'name' in request_args:
-       name = request_args['name']
-   else:
-       name = 'World'
-   return 'Hello {}!'.format(name)
+   return response
