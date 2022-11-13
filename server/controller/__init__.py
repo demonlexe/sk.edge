f54aaf73343b7db3f68c6aadd9d90ae7e8bd0c53
heartgg <a.szumski98@gmail.com>
2022-11-13 01:45:22 -0600

    add professors endpoint

--- server/controller/__init__.py
+++ server/controller/__init__.py
@@ -12,3 +12,18 @@ def get_professor_ctrl(school_name: str, prof_name: str):
         print("Prof", prof_name, "not found!")
         return "Professor not found!", 404
     return prof.__serialize__() 
+
+def get_professors_ctrl(school_name: str, prof_names):
+    school = get_school_by_name(school_name)
+    if school_name is None or prof_names is None:
+        return "Professor/school not provided!", 404
+    if school is None:
+        print("School", school_name, "not found!")
+        return "School not found!", 404
+    prof_list = []
+    for x in prof_names:
+        prof: Professor = get_professor_by_school_and_name(school, x)
+        if prof is None:
+            print("Prof", x, "not found!")
+        prof_list.append(prof.__serialize__())
+    return prof_list
--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
 import functions_framework
 
-from controller import get_professor_ctrl
+from controller import get_professor_ctrl, get_professors_ctrl
 
 @functions_framework.http
 def get_professor(request):
    # request_json = request.get_json(silent=True)
    response = get_professor_ctrl(request.get_json()["school"], request.get_json()["name"])
+
+   return response
+
+@functions_framework.http
+def get_professors(request):
+   # request_json = request.get_json(silent=True)
+   response = get_professors_ctrl(request.get_json()["school"], request.get_json()["name"])
 
    return response
