e7288c122baea925631bd70af35adcb4dc393572
Alexis Kaufman <alexiskaufman@gmail.com>
2022-11-12 22:37:28 -0600

    add routing logic

--- Extension/index.js
+++ Extension/index.js
@@ -1,4 +1,13 @@
 //ADAM WORK HERE
 // Write some logic to decide which html page to actually open
+import { getData, setData } from "../chrome_store.js";
 
-window.location = './setupTab/setupTab.html';
+const isComplete = await getData('user_setup_complete');
+console.log("The value of isComplete is " + isComplete);
+if (isComplete != null && isComplete == true) {
+    // Then user has already completed setup and we have their data.
+    window.location = "./dashboardTab/dashboardTab.html";
+}
+else {
+    window.location = './setupTab/setupTab.html';
+}
