799a39a548fc827966056a80886bffe58e6b29b0
Alexis Kaufman <alexiskaufman@gmail.com>
2022-11-13 04:20:20 -0600

    add more detailed link

--- Extension/professorTab/professorTab.js
+++ Extension/professorTab/professorTab.js
@@ -2,8 +2,8 @@
 const params = new URLSearchParams(document.location.search);
 const professorId = params.get("professorId");
 
-const profName = "jason+smith"; //FIXME with data
+const gradesQuery = "jason+smith+cs+2337"; //FIXME with data
 $("#utd-grades-link").on('click', funct => {
     console.log("Clicked");
-    window.open(`https://utdgrades.com/results?search=${profName}`,'_blank');
+    window.open(`https://utdgrades.com/results?search=${gradesQuery}`,'_blank');
 });
