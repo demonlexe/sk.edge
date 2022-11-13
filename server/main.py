5b6363a4c8ec049bb3b0be98fdd4211aab38a6fb
heartgg <a.szumski98@gmail.com>
2022-11-12 18:08:08 -0600

    change to json

--- server/main.py
+++ server/main.py
@@ -5,6 +5,6 @@ from controller import get_professor_ctrl
 @functions_framework.http
 def get_professor(request):
    # request_json = request.get_json(silent=True)
-   response = get_professor_ctrl(request.args.get("school"), request.args.get("name"))
+   response = get_professor_ctrl(request.get_json()["school"], request.get_json()["name"])
 
    return response
