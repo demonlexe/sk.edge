092ae27b165003f7ba8d040490145f21450c7090
Brad Johnson <2021bradley@gmail.com>
2022-11-13 04:21:47 -0600

    Updating professorTab UI

--- Extension/professorTab/professorTab.js
+++ Extension/professorTab/professorTab.js
@@ -1,3 +1,10 @@
+const data = {
+  professorId: "test",
+  professor: "Jason Smith",
+  rmp: 4.5,
+  grades: [5, 8, 4, 2, 0, 1, 0, 0, 0],
+  endDate: null,
+}
 
 const params = new URLSearchParams(document.location.search);
 const professorId = params.get("professorId");
