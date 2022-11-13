47325703be52bb5298034663009b80776a4b544e
Brad Johnson <2021bradley@gmail.com>
2022-11-13 03:09:29 -0600

    get prof ID

--- Extension/background.js
+++ Extension/background.js
@@ -1,36 +1,37 @@
-
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-	if (/^.*:\/\/utdallas\.collegescheduler\.com\/terms\/.*\/courses\/.+$/.test(details.url)) {
-		chrome.scripting.executeScript({
-			target: {
-				tabId: details.tabId,
-			},
-			files: ["./content-script/index.js"]
-		});
-	}
-	else {
-		// reset the extension popup
-		chrome.action.setPopup({
-			popup: "index.html",
-		})
-	}
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+    if (
+        /^.*:\/\/utdallas\.collegescheduler\.com\/terms\/.*\/courses\/.+$/.test(
+            details.url
+        )
+    ) {
+        chrome.scripting.executeScript({
+            target: {
+                tabId: details.tabId,
+            },
+            files: ["./content-script/index.js"],
+        });
+    } else {
+        // reset the extension popup
+        chrome.action.setPopup({
+            popup: "index.html",
+        });
+    }
 });
 
 const messageType = {
-	SHOW_COURSE_TAB: 'SHOW_COURSE_TAB',
-	SHOW_PROFESSOR_TAB: 'SHOW_PROFESSOR_TAB',
-}
+    SHOW_COURSE_TAB: "SHOW_COURSE_TAB",
+    SHOW_PROFESSOR_TAB: "SHOW_PROFESSOR_TAB",
+};
 
 // listen for messages from the content script
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
     switch (request.type) {
         case messageType.SHOW_COURSE_TAB:
             const { subjectPrefix, courseNumber, professors } = request.payload;
-			chrome.action.setPopup({
-				popup: `./courseTab/courseTab.html?subjectPrefix=${subjectPrefix}&courseNumber=${courseNumber}&professors=${professors.join(",")}`,
-			});
+            chrome.action.setPopup({
+                popup: `./courseTab/courseTab.html?subjectPrefix=${subjectPrefix}&courseNumber=${courseNumber}&professors=${professors.join(",")}`,
+            });
             break;
         case messageType.SHOW_PROFESSOR_TAB:
             // TODO
@@ -38,5 +39,4 @@ chrome.runtime.onMessage.addListener(
         default:
             console.log("Unknown message type");
     }
-  }
-);
+});
---
+++ Extension/professorTab/professorTab.js
@@ -0,0 +1,3 @@
+
+const params = new URLSearchParams(document.location.search);
+const professorId = params.get("professorId");
