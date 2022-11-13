095722b8d4c2eb44b1159716018768b68201566c
Brad Johnson <2021bradley@gmail.com>
2022-11-12 23:10:06 -0600

    Added background script to inject content script, without having to refresh

---
+++ Extension/background.js
@@ -0,0 +1,31 @@
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+	chrome.scripting.executeScript({
+		target: {
+			tabId: details.tabId,
+		},
+		files: ["./content-script/index.js"]
+	});
+});
+
+const messageType = {
+	SHOW_COURSE_TAB: 'SHOW_COURSE_TAB',
+	SHOW_PROFESSOR_TAB: 'SHOW_PROFESSOR_TAB',
+}
+
+// listen for messages from the content script
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request);
+    switch (request.type) {
+        case messageType.SHOW_COURSE_TAB:
+            const { coursePrefix, courseNumber, professors } = request.payload;
+            break;
+        case messageType.SHOW_PROFESSOR_TAB:
+            // TODO
+            break;
+        default:
+            console.log("Unknown message type");
+    }
+  }
+);
